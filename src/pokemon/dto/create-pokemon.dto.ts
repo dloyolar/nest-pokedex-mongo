d79c12e092ab74f41591fd5907208526d08f3410
dloyolar <dieegolr@gmail.com>
2022-11-14 16:48:31 -0300

    ✨ feat(pokemon): add pokemon to db

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePokemonDto {}
+import { IsInt, IsString, Min, MinLength, IsPositive } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
