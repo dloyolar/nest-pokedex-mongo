e4e2a76871e19210243e3644c1b24a60ca7738ad
dloyolar <dieegolr@gmail.com>
2022-11-16 10:54:13 -0300

    🦄 refactor(pokemon): handleExceptions

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -24,15 +24,7 @@ export class PokemonService {
       const pokemon = await this.pokemonModel.create(createPokemonDto);
       return pokemon;
     } catch (error) {
-      if (error.code === 11000) {
-        throw new BadRequestException(
-          `Pokemon exists in db ${JSON.stringify(error.keyValue)}`,
-        );
-      }
-      console.log(error);
-      throw new InternalServerErrorException(
-        `Can't create pokemon - Check server logs`,
-      );
+      this.handleExceptions(error);
     }
   }
 
@@ -70,12 +62,27 @@ export class PokemonService {
 
     if (updatePokemonDto.name) updatePokemonDto.name.toLowerCase();
 
-    await pokemon.updateOne(updatePokemonDto);
-
-    return { ...pokemon.toJSON(), ...updatePokemonDto };
+    try {
+      await pokemon.updateOne(updatePokemonDto);
+      return { ...pokemon.toJSON(), ...updatePokemonDto };
+    } catch (error) {
+      this.handleExceptions(error);
+    }
   }
 
   remove(id: number) {
     return `This action removes a #${id} pokemon`;
+  }
+
+  private handleExceptions(error: any) {
+    if (error.code === 11000) {
+      throw new BadRequestException(
+        `Pokemon exists in db ${JSON.stringify(error.keyValue)}`,
+      );
+    }
+    console.log(error);
+    throw new InternalServerErrorException(
+      `Can't update pokemon - Check server logs`,
+    );
   }
 }
