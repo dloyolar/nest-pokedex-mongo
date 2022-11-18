5eac47c6cfded11bf70356c0041c34a1a42c027e
dloyolar <dieegolr@gmail.com>
2022-11-17 22:04:53 -0300

    ✨ feat(common): optional http adapter with axios

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
-
-@Module({})
+import { AxiosAdapter } from './adapters/axios.adapter';
+import { HttpModule } from '@nestjs/axios';
+@Module({
+  imports: [HttpModule],
+  providers: [AxiosAdapter],
+  exports: [AxiosAdapter],
+})
 export class CommonModule {}
